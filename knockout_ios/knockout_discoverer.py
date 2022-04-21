986ddf5066987a3b119474a68849949042d66aee
linomp <lm2p@hotmail.es>
2022-04-21 17:34:01 +0200

    improve execution time by working with index & .at instead of .loc

--- knockout_ios/knockout_discoverer.py
+++ knockout_ios/knockout_discoverer.py
@@ -176,22 +176,22 @@ class KnockoutDiscoverer:
 
             gr = rejected.groupby(PM4PY_CASE_ID_COLUMN_NAME)
 
-            if not self.quiet:
-                print("\nMarking knocked-out cases in log")
-            for group in tqdm(gr.groups.keys(), desc="Knocked-out cases"):
+            self.log_df.set_index(SIMOD_LOG_READER_CASE_ID_COLUMN_NAME, inplace=True)
+            self.pm4py_formatted_df.set_index(PM4PY_CASE_ID_COLUMN_NAME, inplace=True)
+
+            for group in tqdm(gr.groups.keys(), desc="Marking knocked-out cases in log"):
                 case_df = gr.get_group(group)
                 sorted_case = case_df.sort_values("start_timestamp")
                 knockout_activity = find_ko_activity(self.ko_activities, sorted_case)
 
-                self.log_df.loc[self.log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == group, 'knocked_out_case'] = True
-                self.log_df.loc[
-                    self.log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == group, 'knockout_activity'] = knockout_activity
+                self.log_df.at[group, 'knocked_out_case'] = True
+                self.log_df.at[group, 'knockout_activity'] = knockout_activity
 
-                self.pm4py_formatted_df.loc[
-                    self.pm4py_formatted_df[PM4PY_CASE_ID_COLUMN_NAME] == group, 'knocked_out_case'] = True
-                self.pm4py_formatted_df.loc[
-                    self.pm4py_formatted_df[
-                        PM4PY_CASE_ID_COLUMN_NAME] == group, 'knockout_activity'] = knockout_activity
+                self.pm4py_formatted_df.at[group, 'knocked_out_case'] = True
+                self.pm4py_formatted_df.at[group, 'knockout_activity'] = knockout_activity
+
+            self.log_df.reset_index(inplace=True)
+            self.pm4py_formatted_df.reset_index(inplace=True)
 
             self.log_df.to_pickle(f"./{self.cache_dir}/{self.config_file_name}_with_knockouts.pkl")
             self.pm4py_formatted_df.to_pickle(f"./{self.cache_dir}/{self.config_file_name}_pm4pyf_with_knockouts.pkl")
--- knockout_ios/utils/synthetic_example/attribute_enricher.py
+++ knockout_ios/utils/synthetic_example/attribute_enricher.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from knockout_ios.utils.constants import *
 
@@ -26,45 +27,44 @@ def enrich_log_df(log_df) -> pd.DataFrame:
 
     demographic_values = ['demographic_type_1', 'demographic_type_2', 'demographic_type_3']
 
-    for caseid in log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME].unique():
+    log_df.set_index(SIMOD_LOG_READER_CASE_ID_COLUMN_NAME, inplace=True)
 
-        log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Monthly Income'] = np.random.randint(1000,
-                                                                                                                 4999)
-        log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Total Debt'] = np.random.randint(0, 4999)
-        log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Loan Ammount'] = np.random.randint(0, 9999)
-        log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Owns Vehicle'] = True
-        log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Demographic'] = np.random.choice(
+    for caseid in tqdm(log_df.index.unique(), desc="Enriching with case attributes"):
+
+        log_df.at[caseid, 'Monthly Income'] = np.random.randint(1000,
+                                                                4999)
+        log_df.at[caseid, 'Total Debt'] = np.random.randint(0, 4999)
+        log_df.at[caseid, 'Loan Ammount'] = np.random.randint(0, 9999)
+        log_df.at[caseid, 'Owns Vehicle'] = True
+        log_df.at[caseid, 'Demographic'] = np.random.choice(
             demographic_values)
-        log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'External Risk Score'] = np.random.uniform(0,
-                                                                                                                      0.3)
-        log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Aggregated Risk Score'] = np.random.uniform(
+        log_df.at[caseid, 'External Risk Score'] = np.random.uniform(0,
+                                                                     0.3)
+        log_df.at[caseid, 'Aggregated Risk Score'] = np.random.uniform(
             0, 0.49)
 
-        ko_activity = log_df[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid]["knockout_activity"]
+        ko_activity = log_df.loc[caseid]["knockout_activity"].unique()
         if len(ko_activity) > 0:
-            ko_activity = ko_activity.iloc[0]
+            ko_activity = ko_activity[0]
 
         if ko_activity == 'Check Liability':
             if np.random.uniform(0, 1) < 0.5:
-                log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Total Debt'] = np.random.randint(
+                log_df.at[caseid, 'Total Debt'] = np.random.randint(
                     5000, 30_000)
             else:
-                log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Owns Vehicle'] = False
+                log_df.at[caseid, 'Owns Vehicle'] = False
         elif ko_activity == 'Check Risk':
-            log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Loan Ammount'] = np.random.randint(
+            log_df.at[caseid, 'Loan Ammount'] = np.random.randint(
                 10_000, 30_000)
         elif ko_activity == 'Check Monthly Income':
-            log_df.loc[log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Monthly Income'] = np.random.randint(0,
-                                                                                                                     999)
+            log_df.at[caseid, 'Monthly Income'] = np.random.randint(0,
+                                                                    999)
         elif ko_activity == 'Assess application':
-            log_df.loc[
-                log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'External Risk Score'] = np.random.uniform(0.3,
-                                                                                                                   1)
+            log_df.at[caseid, 'External Risk Score'] = np.random.uniform(0.3, 1)
         elif ko_activity == 'Aggregated Risk Score Check':
-            log_df.loc[
-                log_df[SIMOD_LOG_READER_CASE_ID_COLUMN_NAME] == caseid, 'Aggregated Risk Score'] = np.random.uniform(
-                0.5, 1)
+            log_df.at[caseid, 'Aggregated Risk Score'] = np.random.uniform(0.5, 1)
 
+    log_df.reset_index(inplace=True)
     return log_df
 
 
