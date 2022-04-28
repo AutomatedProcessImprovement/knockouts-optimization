bcd74f83ef7d043d95d19d363a08d434436ce942
linomp <lm2p@hotmail.es>
2022-04-28 19:34:24 +0200

    tests ready

--- knockout_ios/test/test_redesign_utils.py
+++ knockout_ios/test/test_redesign_utils.py
@@ -1,9 +1,11 @@
-from collections import Counter
-
-import pandas as pd
 import pytest
 
-from knockout_ios.utils.constants import SIMOD_LOG_READER_CASE_ID_COLUMN_NAME, SIMOD_END_TIMESTAMP_COLUMN_NAME
+import pandas as pd
+from pandas import Timestamp
+
+from collections import Counter
+
+from knockout_ios.utils.constants import *
 from knockout_ios.utils.redesign import get_sorted_with_dependencies, find_producers
 
 
@@ -30,39 +32,179 @@ def test_get_sorted_with_dependencies_2():
     assert optimal_order == ["B", "C", "A"]
 
 
-@pytest.mark.skip(reason="TODO: create traces for test")
 def test_find_producer_activity_simple():
-    attribute = "attr_produced_by_B"
+    attribute_key = "attr_produced_by_B"
     ko_activity = "D"
 
     # in this case, the attribute stops being null from activity B, and never changes since then.
     # therefore, B is considered the producer.
-    # TODO: create traces for test
-    log = []
-    log = pd.DataFrame(log)
-    log.set_index(SIMOD_LOG_READER_CASE_ID_COLUMN_NAME, inplace=True)
-    log.sort_values(by=SIMOD_END_TIMESTAMP_COLUMN_NAME)
+    events = [
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'Start',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 08:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 09:00:00"),
+            attribute_key: None
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'A',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 09:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 10:00:00"),
+            attribute_key: None
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'B',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 10:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 11:00:00"),
+            attribute_key: None
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'C',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 11:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 12:00:00"),
+            attribute_key: "SOME_VALUE"
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: ko_activity,
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 12:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 13:00:00"),
+            attribute_key: "SOME_VALUE"
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'End',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 13:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 14:00:00"),
+            attribute_key: "SOME_VALUE"
+        }
+    ]
 
-    producers = find_producers(attribute, ko_activity, log)
+    log = pd.DataFrame(events)
+    log.set_index(SIMOD_LOG_READER_CASE_ID_COLUMN_NAME, inplace=True)
+    log.sort_values(by=SIMOD_END_TIMESTAMP_COLUMN_NAME, inplace=True)
+
+    producers = find_producers(attribute_key, ko_activity, log)
 
     assert len(producers) > 0
-    assert Counter(producers).most_common(1)[0][0] == ["B"]
+    assert Counter(producers).most_common(1)[0][0] == "B"
 
 
-@pytest.mark.skip(reason="TODO: create traces for test")
 def test_find_producer_activity_advanced():
-    attribute = "attr_produced_by_D"
+    attribute_key = "attr_produced_by_D"
     ko_activity = "F"
 
     # in this case, the attribute stops being null from activity B, but keeps changing until activity D.
     # therefore, D is considered the producer.
-    # TODO: create traces for test
-    log = []
-    log = pd.DataFrame(log)
+    events = [
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'Start',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 08:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 09:00:00"),
+            attribute_key: None
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'A',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 09:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 10:00:00"),
+            attribute_key: None
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'B',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 10:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 11:00:00"),
+            attribute_key: None
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'C',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 11:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 12:00:00"),
+            attribute_key: "B"
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: "D",
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 12:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 13:00:00"),
+            attribute_key: "B,C"
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: "E",
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 13:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 14:00:00"),
+            attribute_key: "B,C,D"
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: "F",
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 13:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 14:00:00"),
+            attribute_key: "B,C,D"
+        },
+        {
+            SIMOD_LOG_READER_CASE_ID_COLUMN_NAME: 0,
+            'knockout_activity': ko_activity,
+            'knocked_out_case': True,
+            SIMOD_LOG_READER_ACTIVITY_COLUMN_NAME: 'End',
+            SIMOD_RESOURCE_COLUMN_NAME: 'R1',
+            SIMOD_START_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 13:00:00"),
+            SIMOD_END_TIMESTAMP_COLUMN_NAME: Timestamp("2022-02-17 14:00:00"),
+            attribute_key: "B,C,D"
+        }
+    ]
+
+    log = pd.DataFrame(events)
     log.set_index(SIMOD_LOG_READER_CASE_ID_COLUMN_NAME, inplace=True)
     log.sort_values(by=SIMOD_END_TIMESTAMP_COLUMN_NAME)
 
-    producers = find_producers(attribute, ko_activity, log)
+    producers = find_producers(attribute_key, ko_activity, log)
 
     assert len(producers) > 0
-    assert Counter(producers).most_common(1)[0][0] == ["D"]
+    assert Counter(producers).most_common(1)[0][0] == "D"
