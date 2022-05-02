376230b95743530040217eadc4bfb23335241955
linomp <lm2p@hotmail.es>
2022-05-03 01:07:12 +0200

    added modified synthetic example log for showcase of capabilities

--- knockout_ios/knockout_analyzer.py
+++ knockout_ios/knockout_analyzer.py
@@ -205,7 +205,7 @@ class KnockoutAnalyzer:
                          k=2,
                          n_discretize_bins=10,
                          dl_allowance=16,
-                         prune_size=0.33,
+                         prune_size=0.95,
                          grid_search=False,
                          param_grid=None,
                          confidence_threshold=0.5,
@@ -239,7 +239,7 @@ class KnockoutAnalyzer:
                 param_grid = {"prune_size": [0.2, 0.33, 0.5], "k": [1, 2, 4], "dl_allowance": [16, 32, 64],
                               "n_discretize_bins": [10, 20, 30]}
             elif algorithm == "IREP":
-                param_grid = {"prune_size": [0.2, 0.33, 0.5], "n_discretize_bins": [10, 20, 30]}
+                param_grid = {"prune_size": [0.5, 0.7, 0.95], "n_discretize_bins": [10, 20, 30]}
 
         try:
             rulesets = find_ko_rulesets(self.rule_discovery_log_df,
--- knockout_ios/utils/synthetic_example/preprocessors.py
+++ knockout_ios/utils/synthetic_example/preprocessors.py
@@ -64,3 +64,32 @@ def enrich_log_for_ko_order_advanced_test(ko_analyzer: 'KnockoutAnalyzer',
 def enrich_log_for_ko_order_advanced_test_fixed_values_wrapper(ko_analyzer: 'KnockoutAnalyzer', log: pd.DataFrame,
                                                                cache_file_path: str = None) -> pd.DataFrame:
     return enrich_log_for_ko_order_advanced_test(ko_analyzer, log, cache_file_path, fixed_values=True)
+
+
+def enrich_log_for_synthetic_example_validation(ko_analyzer: 'KnockoutAnalyzer',
+                                                log: pd.DataFrame,
+                                                cache_file_path: str = None, fixed_values=False) -> pd.DataFrame:
+    '''
+    Returns the log enriched with attributes whose values are known only after a certain activity has been performed,
+    in particular, the 'Aggregated Risk Score' can be computed after activity 'Check Risk' and is needed by the activity
+    'Aggregated Risk Score Check'
+    '''
+
+    # TODO: remove this if the .xes exporting problem is ever fixed - not critical anyway.
+    if "synthetic_example" in ko_analyzer.config_file_name:
+        try:
+            if ko_analyzer.always_force_recompute:
+                raise FileNotFoundError
+
+            return pd.read_pickle(cache_file_path)
+
+        except FileNotFoundError:
+            enriched_log = enrich_log_df_with_masked_attributes(log,
+                                                                [RuntimeAttribute(
+                                                                    attribute_name='External Risk Score',
+                                                                    value_provider_activity='Check Risk')
+                                                                ],
+                                                                fixed_values=fixed_values)
+            enriched_log.to_pickle(cache_file_path)
+
+            return enriched_log
